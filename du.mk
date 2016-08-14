8551d25156d49f237c83564c918c717ee04c0053
Shreesha Murthy <shreeshu10@gmail.com>
2016-08-14 06:27:18 +0000

    DU-ify

--- du.mk
+++ du.mk
@@ -14,5 +14,5 @@
 
 $(call inherit-product, device/oneplus/oneplus2/full_oneplus2.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common du stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
