a479d55803c6f175518dc9d85b1a044fcd5b81d4
Shreesha <shreeshu10@gmail.com>
2016-12-01 22:13:55 +0530

    remove cmhw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -209,8 +209,7 @@ BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
-# CM Hardware
-BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
+# D2TW
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # inherit from the proprietary version
