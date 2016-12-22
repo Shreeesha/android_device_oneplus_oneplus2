128b0a57ac6a909a545495539cb21a0d5fea38bf
Shreesha <shreeshu10@gmail.com>
2016-12-22 12:13:48 -0500

    Enable dexpreopt for userdebug builds
    Change-Id: I454ad7a29713d6531843c4c7dd2ebbe5c97ace3b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,10 +161,10 @@ USE_SENSOR_MULTI_HAL := true
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
-    endif
+   endif
   endif
 endif
 
