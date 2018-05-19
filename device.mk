d5c90644e334da4c117a74c5afd5bedde9bec5ba
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2018-05-19 08:40:45 +0200

    op2: remove perms for unused midi feature
    Change-Id: I4c6d35bb9da680111fe390166483c4763c9727dc

--- device.mk
+++ device.mk
@@ -233,10 +233,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
-# MIDI feature
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
-
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
